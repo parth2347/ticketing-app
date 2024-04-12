a88d823c1641b7b77ea7740c6b5ad58b04534499
Parth <parthparekh407@gmail.com>
2024-04-11 18:54:14 -0700

    adding routes and errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentuserRouter } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { siginRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
+app.use(currentuserRouter);
+app.use(signoutRouter);
+app.use(siginRouter);
+app.use(signupRouter);
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
