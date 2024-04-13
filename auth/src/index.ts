e95aa5d36ae09035cba688fa0a5cc9a6bfc37cd9
Parth <parthparekh407@gmail.com>
2024-04-13 11:54:15 -0700

    Handling errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ import { signoutRouter } from "./routes/signout";
 import { siginRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import "express-async-errors";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
@@ -13,8 +16,23 @@ app.use(signoutRouter);
 app.use(siginRouter);
 app.use(signupRouter);
 
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
