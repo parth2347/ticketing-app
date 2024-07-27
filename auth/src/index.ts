7558f30df94127129e2cf9950491a30c0114905a
Parth <39831689+parth2347@users.noreply.github.com>
2024-07-26 23:40:50 -0400

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import 'express-async-errors';
 import { json } from "body-parser";
 import { currentuserRouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -22,7 +22,7 @@ router.post(
 
     const { email, password } = req.body;
     console.log("Creating a user ...");
-    throw new DatabaseConnectionError();
+    return res.send({})
   }
 );
 
